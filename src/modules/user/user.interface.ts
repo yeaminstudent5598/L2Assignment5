03f82781d61ece282a9f37cbce6d31d036bab9ec
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-08-27 23:33:38 +0600

    implement server-side pagination, search, and filtering

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -1,7 +1,7 @@
 // D:\yeamin student\Programming hero level 2\Assignment5\src\modules\user\user.interface.ts
 
 export type UserRole = 'ADMIN' | 'SENDER' | 'RECEIVER';
-
+ 
 
 export interface IUser {
   _id?: string;            
