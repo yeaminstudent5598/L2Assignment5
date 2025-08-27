0917266dd6853afdb699e98198aa762b532f540c
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-08-27 23:31:59 +0600

    implement role-specific parcel retrieval logic

--- src/modules/dashboard/dashboard.controller.ts
+++ src/modules/dashboard/dashboard.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
-import sendResponse from '../../utils/sendResponse';
+import sendResponse from '../../utils/sendResponse'; 
 import { DashboardService } from './dashboard.service';
 import httpStatus from 'http-status-codes';
 import AppError from '../../errorHelpers/AppError';
